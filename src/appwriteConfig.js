99d1e11f05807be6ad67aa83d9b76f1ae3c6d215
moarnight <moarnight@gmail.com>
2023-08-09 16:50:49 +0200

    Configure Appwrite

---
+++ src/appwriteConfig.js
@@ -0,0 +1,9 @@
+import { Client } from 'appwrite';
+
+const client = new Client();
+
+client
+  .setEndpoint(process.env.APPWRITE_ENDPOINT)
+  .setProject(process.env.APPWRITE_PROJECT);
+
+export default client;
