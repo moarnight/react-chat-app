3eed792ddfb785685ba00466ab5ae071473a145f
moarnight <moarnight@gmail.com>
2023-08-10 11:04:02 +0200

    Query database items with listDocuments

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,9 +1,16 @@
-import { Client } from 'appwrite';
+import { Client, Databases } from 'appwrite';
+
+export const PROJECT_ID = process.env.REACT_APP_APPWRITE_PROJECT_ID;
+export const DATABASE_ID = process.env.REACT_APP_APPWRITE_DATABASE_ID;
+export const COLLECTION_ID_MESSAGES =
+  process.env.REACT_APP_APPWRITE_COLLECTION_ID_MESSAGES;
 
 const client = new Client();
 
 client
-  .setEndpoint(process.env.APPWRITE_ENDPOINT)
-  .setProject(process.env.APPWRITE_PROJECT);
+  .setEndpoint(process.env.REACT_APP_APPWRITE_ENDPOINT)
+  .setProject(process.env.REACT_APP_APPWRITE_PROJECT);
+
+export const databases = new Databases(client);
 
 export default client;
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  databases,
+  DATABASE_ID,
+  COLLECTION_ID_MESSAGES,
+} from '../appwriteConfig';
 
 const Room = () => {
+  useEffect(() => {
+    getMessages();
+  }, []);
+
+  const getMessages = async () => {
+    const response = await databases.listDocuments(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGES
+    );
+    console.log('RESPONSE:', response);
+  };
+
   return <div>Room</div>;
 };
 
