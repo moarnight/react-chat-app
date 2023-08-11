2869e00d1b4a13c4c56abea989a5cab27d3e543a
moarnight <moarnight@gmail.com>
2023-08-11 13:51:37 +0200

    Add routing and set up protected routes

---
+++ src/components/PrivateRoutes.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Outlet, Navigate } from 'react-router-dom';
+
+const PrivateRoutes = () => {
+  const user = true;
+  return <>{user ? <Outlet /> : <Navigate to="/login" />}</>;
+};
+
+export default PrivateRoutes;
