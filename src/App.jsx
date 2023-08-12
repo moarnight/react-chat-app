103f56cb2ccd33be1821f8e98d19eacf34b0c44f
moarnight <moarnight@gmail.com>
2023-08-12 17:51:47 +0200

    Add AuthContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PrivateRoutes from './components/PrivateRoutes';
+import { AuthProvider } from './utilities/AuthContext';
 import Room from './pages/Room';
 import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route element={<PrivateRoutes />}>
-          <Route path="/" element={<Room />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route element={<PrivateRoutes />}>
+            <Route path="/" element={<Room />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAuth } from '../utilities/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate('/');
+    }
+  }, []);
+
   return <div>LoginPage</div>;
 };
 
